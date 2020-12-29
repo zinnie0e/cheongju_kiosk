ebe0c41872283b92a05c6589b3bbf450074309e5
GURU.DEV.PJE <jepark@guruiot.com>
2020-12-29 18:40:19 +0900

    merge pje

--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -4,19 +4,54 @@
 <html>
 <head>
 	<link rel="stylesheet" href="./resources/css/main.css" type="text/css" />
+	<link rel="stylesheet" href="./resources/css/event.css" type="text/css" />
+	<link rel="stylesheet" href="./resources/css/area0.css" type="text/css" />
 	<link rel="stylesheet" href="./resources/css/area1.css" type="text/css" />
 	<link rel="stylesheet" href="./resources/css/area1_0.css" type="text/css" />
+	<link rel="stylesheet" href="./resources/css/area1_1.css" type="text/css" />
+	<link rel="stylesheet" href="./resources/css/area4.css" type="text/css" />
+	
+	<!-- 슬라이더 라이브러리 -->
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+	<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 	
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 	
 	<script src="./resources/js/main.js"></script>
+	<script src="./resources/js/event.js"></script>
+	<script src="./resources/js/area0.js"></script>
 	<script src="./resources/js/area1.js"></script>
 	<script src="./resources/js/area1_0.js"></script>
+	<script src="./resources/js/area1_1.js"></script>
+	<script src="./resources/js/area1_2.js"></script>
+	<script src="./resources/js/area1_3.js"></script>
+	<script src="./resources/js/area1_4.js"></script>
+	<script src="./resources/js/area2.js"></script>
+	<script src="./resources/js/area3.js"></script>
+	<script src="./resources/js/area4.js"></script>
 	
 	<title>Home</title>
 </head>
-<body>
-	<div id="div_top"></div>
+<body onclick="javascript:resetTimer();">
+	<div id="div_top">
+		<div id="ticker_top" style="position:absolute; width: 1000px; height: 110px; margin-top: 60px; margin-left: 40px;">
+			<div id="ticker_left" style="width: 75%; height: 100%; float: left;">
+				<div id="ticker_temperature_icon"></div>
+				<div id="ticker_right" style="width: 640px; height: 100%; float: left;">
+					<div id="ticker_date"></div>
+					<div id="ticker_bottom" style="width: 600px; height: 45px; margin-left: 40px; margin-top:20px;">
+						<div id="ticker_temperature"><a id="ticker_temperature_min"></a><a style="height:45px;">&nbsp;/&nbsp;</a><a id="ticker_temperature_max"></a></div>
+						<div id="ticker_finedust">미세먼지</div>
+						<div id="ticker_finedust_icon"></div>
+					</div>
+				</div>
+			</div>
+			<div id="ticker_time"></div>
+		</div>
+		<div id="ticker_notice"><p><MARQUEE id="ticker_notice_ment" behavior="scroll"></MARQUEE></p></div>
+	</div>
 	<div id="div_main">
 		<div id="div_promotion"></div>
 		<div id="div_contents"></div>
--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -9,7 +9,25 @@ function logNow(logContents){
 $(document).ready(function(){
 	initKiosk();
 	initJsonPromotion();
+	resetTimer();
+	startTimer();
 });
+
+var initSeconds = 60; // 타이머 초
+var remainSeconds;
+function resetTimer(){ // 타이머 초기화 함수
+	remainSeconds = initSeconds; 
+}
+
+function startTimer(){ // 타이머 시작 함수
+	logNow(remainSeconds);
+	if(remainSeconds > 0){
+		remainSeconds -= 1;
+		setTimeout("startTimer()",1000); //1초간격으로 재귀호출
+	}else{
+		history.go(0);
+	}
+}
 
 var global_json = null;
 function initJson(language) {
@@ -49,11 +67,67 @@ function initPromotion(play) {
 			if(promotion_num >= promotion_json.length){
 				promotion_num = 0;
 			}
-		}, 6 * 1000);
+		}, 15 * 1000);
 	} else {
 		clearInterval(promotion_timer);
 		$('#div_promotion').hide();
 	}
+}
+
+function getTemperature(){ //기온
+    $.getJSON( "./resources/apival/temperature.jsp", function( data ) {
+    	var maxtem; var mintem;
+    	var wea_info = 'url(./resources/image/main/weather_icon_';
+
+        if(data != undefined && !isNaN(data.mintem) && !isNaN(data.maxtem) && !isNaN(data.weaCode)) {
+        	if(data.mintem == '-999') mintem = '-'; else mintem = data.mintem + '°';
+        	if(data.maxtem == '-999') maxtem = '-'; else maxtem = data.maxtem + '°';
+        	
+        	switch(data.weaCode){
+        		case 1:
+        			wea_info += 'sunny.png)';
+        			break;
+        		case 2:
+        			wea_info += 'sunny-cloud.png)';
+        			break;
+        		case 3:
+        			wea_info += 'cloudy.png)';
+        			break;
+        		case 4:
+        			wea_info += 'rain.png)';
+        			break;
+        		case 5:
+        			wea_info += 'snow-rain.png)';
+        			break;
+        		case 6:
+        			wea_info += 'snow.png)';
+        			break;
+        	}
+        } else {
+        	logNow("날씨 데이터 오류");
+        }
+        document.getElementById("ticker_temperature_min").innerHTML = mintem;
+        document.getElementById("ticker_temperature_max").innerHTML = maxtem;
+        $('#ticker_temperature_icon').css('background-image', wea_info);
+    });
+}
+
+function setTime() { //날짜 및 시간
+	var d = new Date();
+    document.getElementById("ticker_time").innerHTML = leadZero(d.getHours(),2) + ':' + leadZero(d.getMinutes(),2);
+	document.getElementById("ticker_date").innerHTML = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate() + ' ' + getDayOfWeek(d.getDay());
+}
+
+function getDayOfWeek(dow){
+    return global_json.dayOfWeek[dow];
+}
+
+//leading zero 만들기
+function leadZero(input,digits){
+    for(var cur = (input + '').length; cur < digits; cur++){
+        input = '0' + input;
+    }
+    return input;
 }
 
 function initKiosk() {
@@ -64,10 +138,15 @@ function initKiosk() {
 	$('#div_side').hide();
 	$('#img_shadow').hide();
 	
+	if(language == null)
+		language = "korean";
 	
 	$('#div_main').attr('onclick', 'initMain();');
 	
-	initJson("korean");
+	getTemperature();
+	initJson(language);
+	document.getElementById("ticker_notice_ment").innerHTML = global_json.ticker_notice_ment;
+	setTime();
 	initJsonPromotion();
 }
 
@@ -89,30 +168,41 @@ function showSideBottom() {
 	var html_string = "";
 	
 	html_string += '<div id="div_side_back" class="div_side_bottom" onclick="javascript:backPage(0);"></div>';
-	html_string += '<div id="div_side_home" class="div_side_bottom" onclick="javascript:history.go(0);"></div>';
+	html_string += '<div id="div_side_home" class="div_side_bottom" onclick="javascript:initMain();"></div>';
 	
 	$('#div_side_bottom').html(html_string);
 }
 
 function showMain() {
-	var html_string = '<div id="div_language">';
+	var html_string = '<div id="div_milestone">';
+	for(var i = 0; i < 5; i++){
+		html_string += '<div id="div_milestone' + i + '" class="div_milestone" onclick="javascript:setMainArea(' + i + ');"></div>';
+		html_string += '<div id="div_milestone_sub' + i + '" onclick="javascript:setMainArea(' + i + ');"></div>';
+	}
+	html_string += '</div>';
+	
+	html_string += '<div id="div_language">';
 	for(var i = 0; i < 4; i++){
 		html_string += '<div id="div_language' + i + '" class="div_language" onclick="javascript:setMainLanguage(' + i + ');"></div>';
 	}
 	html_string += '</div>';
 	
-	for(var i = 0; i < 5; i++){
-		html_string += '<div id="div_area' + i + '" onclick="javascript:setMainArea(' + i + ');"></div>';
-	}
-	
 	$('#div_contents').html(html_string);
-	$('#div_contents').css('background-image', 'url(./resources/image/temp_main.png)');
+	if(language == null)
+		language = "korean";
+	$('#div_contents').css('background-image', 'url(./resources/image/main/main_map.png)');
+	$('#div_language').css('background-image', global_json.main_language);
+	$('#div_milestone0').css('background-image', global_json.btn_main_map[0]);
+	$('#div_milestone1').css('background-image', global_json.btn_main_map[1]);
+	$('#div_milestone2').css('background-image', global_json.btn_main_map[2]);
+	$('#div_milestone3').css('background-image', global_json.btn_main_map[3]);
+	$('#div_milestone4').css('background-image', global_json.btn_main_map[4]);
 }
 
 function setMainArea(index_num) {
 	switch (index_num) {
 		case 0: {
-			alert("area0");
+			showArea0(language);
 			break;
 		}
 		case 1: {
@@ -120,15 +210,15 @@ function setMainArea(index_num) {
 			break;
 		}
 		case 2: {
-			alert("area2");
+			showArea2();
 			break;
 		}
 		case 3: {
-			alert("area3");
+			showArea3();
 			break;
 		}
 		case 4: {
-			alert("area4");
+			showArea4();
 			break;
 		}
 	}
@@ -160,7 +250,13 @@ function showSideTop(index_num) {
 			$('#div_side_top').css('background-image', 'url(./resources/image/temp_area1_0_side_top.png)');
 			break;
 		}
+		//첨단문화산업단지
 		case 2: {
+			for(var i = 0; i <= 6; i++){
+				html_string += '<div id="div_side_area0_' + i + '" class="div_side_area0" onclick="javascript:setSideArea0(' + i + ');"></div>';
+			}
+			
+			$('#div_side_top').css('background-image', 'url(./resources/image/industry_side_top.png)');
 			break;
 		}
 		case 3: {
@@ -179,19 +275,22 @@ function resetSide(){
 
 function setSide(document){
 	resetSide();
-	$(document).css('background-color', 'aqua');
+	//$(document).css('background-color', 'aqua');
 }
 
 function setMainSide(document, index_num) {
 	setSide(document);
 	switch (index_num) {
-		case 0: {
-			alert("이벤트");
+		case 0: { 
+			//진행중 이벤트
+			initEvent();
 			break;
 		}
 		case 1: {
+			showSideTop(1);
 			$('#div_contents').html('');
 			$('#div_contents').css('background-image', 'url(./resources/image/temp_operating.png)');
+			$('#div_main_side1').css('background-image', 'url(./resources/image/temp_operating_sel.png)');
 			break;
 		}
 		case 2: {
@@ -203,25 +302,30 @@ function setMainSide(document, index_num) {
 	backPage(1);
 }
 
+var language = null;
 function setMainLanguage(index_num) {
+	
+	html_string = '<div id="div_main_language"></div>';
 	switch (index_num) {
 		case 0: {
-			alert("kr");
+			language = "korean";
 			break;
 		}
 		case 1: {
-			alert("en");
+			language = "english";
 			break;
 		}
 		case 2: {
-			alert("ch");
+			language = "chinese";
 			break;
 		}
 		case 3: {
-			alert("jp");
+			language = "japanese";
 			break;
 		}
 	}
+	initJson(language);
+	initMain();
 }
 
 function backPage(current_depth){
@@ -236,6 +340,18 @@ function backPage(current_depth){
 		}
 		case 2: {
 			$('#div_side_back').attr('onclick', 'showArea1();');
+			break;
+		}
+		case 3: {
+			$('#div_side_back').attr('onclick', 'setMainArea(0);');
+			break;
+		}
+		case 4: {
+			$('#div_side_back').attr('onclick', 'setArea1(this,1);');
+			break;
+		}
+		case 5: {
+			$('#div_side_back').attr('onclick', 'setArea1(this, 0)');
 			break;
 		}
 	}
