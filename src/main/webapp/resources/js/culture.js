bb3e75eb5933bce3b7d54be9436e3970e39fa59d
GURU.DEV.LWH <GURU.DEV.LWH@DESKTOP-SS8OD7I>
2021-01-04 15:48:49 +0900

    merge lwh

--- src/main/webapp/resources/js/culture.js
+++ src/main/webapp/resources/js/culture.js
@@ -2,12 +2,14 @@ function showArea1() {
 	showSideTop(1);
 	
 	var html_string = "";
-	for(var i = 0; i < 5; i++){
-		html_string += '<div id="div_area1_' + i + '" onclick="javascript:setArea1(this, ' + i + ');"></div>';
+	html_string += '<div id="div_area1">';
+	for(var i = 0; i < 4; i++){
+		html_string += '<div class="div_area1" onclick="javascript:setArea1(this, ' + i + ');"></div>';
 	}
+	html_string += '</div>';
 	
 	$('#div_contents').html(html_string);
-	$('#div_contents').css('background-image', 'url(./resources/image/culture/temp_area1.png)');
+	$('#div_contents').css('background-image', 'url('+ global_json.culture +')');
 	backPage(1);
 }
 
@@ -25,13 +27,13 @@ function setArea1(document, index_num) {
 			break;
 		}
 		case 2: {
-			//열린도서관
-			showArea1_2();
+			//미디어재단
+			showArea1_3();
 			break;
 		}
 		case 3: {
-			//미디어재단
-			showArea1_3();
+			//열린도서관
+			showArea1_2();
 			break;
 		}
 		case 4: {
